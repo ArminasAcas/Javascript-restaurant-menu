6fe9c4343b72669ad98577cd9a02a3eaa3d49e0c
ArminasAcas <arminasprollas@gmail.com>
2023-08-08 23:32:10 +0300

    added global variables

---
+++ javascript/Global.js
@@ -0,0 +1,32 @@
+let categories = {
+    main_course: "Main course",
+    side_dish: "Side dish",
+    salads: "Salads",
+    soups: "Soups",
+    desserts: "Desserts",
+    beverages: "Beverages",
+}
+
+let items = [
+    {
+        imgSource: "images/french-fries.jpg",
+        name: "French fries",
+        description: "French fries made with fresh, hand-cut potatoes and cooked to perfection in our signature blend of oils",
+        price: 1.8,
+        category: categories.side_dish
+    },
+    {
+        imgSource: "images/french-fries.jpg",
+        name: "French fries",
+        description: "French fries made with fresh, hand-cut potatoes and cooked to perfection in our signature blend of oils",
+        price: 1.8,
+        category: categories.side_dish
+    },
+    {
+        imgSource: "images/french-fries.jpg",
+        name: "French fries",
+        description: "French fries made with fresh, hand-cut potatoes and cooked to perfection in our signature blend of oils",
+        price: 1.8,
+        category: categories.side_dish
+    },
+]
