2cd3660691181c6fd9793e0470d593102876a707
ArminasAcas <arminasprollas@gmail.com>
2023-08-09 23:44:35 +0300

    added menu section item display

--- javascript/MenuSections.js
+++ javascript/MenuSections.js
@@ -3,18 +3,39 @@ function menuSections() {
     let itemsDiv = document.querySelector(".items");
     if (!itemsDiv) return;
 
+    let menuSection = document.querySelector(".menu-sections");
+    if (!menuSection) return;
+
+    createButtons(menuSection);
     createItems(itemsDiv);
     
     let buttons = document.querySelectorAll(".button");
     if (!buttons) return;
 
+    let items = document.querySelectorAll(".item");
+    if (!items) return;
+
+    displayCategoryItems(categories.side_dish, items);
+
     buttons.forEach(button => {
         button.addEventListener("click", () => {
-            
+            displayCategoryItems(button.getAttribute("data-category"), items);
         })
     })
 }
 
+function createButtons(menuSection) {
+    let categoryArray = Object.values(categories);
+    categoryArray.forEach(section => {
+        let button = document.createElement("button");
+        button.innerHTML = section;
+        button.classList.add("button");
+        button.setAttribute("data-category", section);
+
+        menuSection.appendChild(button);
+    
+    })
+}
 function createItems(itemsDiv) {
     itemArray.forEach((item,index) => {
         let itemDiv = document.createElement("div");
@@ -45,6 +66,17 @@ function createItems(itemsDiv) {
         itemDiv.appendChild(img);
         itemDiv.appendChild(namePriceDiv);
         itemDiv.appendChild(description);
+        itemDiv.setAttribute('data-category',item.category);
         itemsDiv.appendChild(itemDiv);
+    })
+}
+
+function displayCategoryItems(section, items){
+    items.forEach(item => {
+        console.log(section);
+        console.log(item.getAttribute("data-category"));
+        console.log("-----");
+        if (item.getAttribute("data-category") !== section) item.style.display = "none";
+        else if (item.getAttribute("data-category") === section) item.style.display = "block";
     })
 }
