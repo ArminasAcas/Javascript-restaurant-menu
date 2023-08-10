921258c5fce4527f4fe44c3c83d67357c087c985
ArminasAcas <arminasprollas@gmail.com>
2023-08-10 23:20:14 +0300

    added extra menu items

--- javascript/Global.js
+++ javascript/Global.js
@@ -10,14 +10,14 @@ let categories = {
 let itemArray = [
     {
         imgSource: "images/french-fries.jpg",
-        name: "French fries",
+        name: "French Fries",
         description: "French fries made with fresh, hand-cut potatoes and cooked to perfection in our signature blend of oils",
         price: 1.8,
         category: categories.side_dish
     },
     {
         imgSource: "images/vegetable-salad.jpg",
-        name: "Vegetable salad",
+        name: "Vegetable Salad",
         description: "Refreshing and colorful salad made with a variety of fresh vegetables",
         price: 2.2,
         category: categories.salads
@@ -35,5 +35,82 @@ let itemArray = [
         description: "Cooked to perfection in hot oil, resulting in a crispy skin and juicy, flavorful meat",
         price: 5.5,
         category: categories.main_course
+    },
+    {
+        imgSource: "images/Classic-lasagna.jpg",
+        name: "Classic Lasagna",
+        description: "Baked to perfection and served hot and bubbly",
+        price: 8,
+        category: categories.main_course
+    },
+    {
+        imgSource: "images/Classic Italian Meatballs.jpg",
+        name: "Classic Italian Meatballs",
+        description: "Made with a blend of ground beef, pork, and veal, along with fresh herbs, garlic and breadcrumbs",
+        price: 5.4,
+        category: categories.side_dish
+    },
+    {
+        imgSource: "images/Caesar-salad.jpg",
+        name: "Caesar Salad",
+        description: "Made with crisp romaine lettuce, creamy Caesar dressing and topped with Parmesan cheese",
+        price: 3.8,
+        category: categories.salads
+    },
+    {
+        imgSource: "images/Greek salad.jpg",
+        name: "Greek Salad",
+        description: "Made with tomatoes, cucumbers, olives, feta, and onions and lightly dressed with olive oil, oregano",
+        price: 4.1,
+        category: categories.salads
+    },
+    {
+        imgSource: "images/Roasted-vegetables.jpg",
+        name: "Roasted Vegetables",
+        description: "Delicious and healthy side dish that is perfect for any meal",
+        price: 1.75,
+        category: categories.side_dish
+    },
+    {
+        imgSource: "images/Tortilla-soup.jpg",
+        name: "Tortilla Soup",
+        description: "Pieces of fried corn tortillas mixed with a tomato broth and seasoned with garlic",
+        price: 3,
+        category: categories.soups
+    },
+    {
+        imgSource: "images/Wonton-soup.jpg",
+        name: "Wonton Soup",
+        description: "Very fine dough made with flour, eggs and cooked in a spicy broth with vegetables and meat",
+        price: 3.5,
+        category: categories.soups
+    },
+    {
+        imgSource: "images/Cornflake-corn-cake.jpg",
+        name: "Cornflake Corn Cake",
+        description: "A cake, with a texture that is just amazing",
+        price: 2.65,
+        category: categories.desserts
+    },
+    {
+        imgSource: "images/Cherry-crunch-dessert.jpg",
+        name: "Cherry Crunch Dessert",
+        description: "Made for cheesecake lovers",
+        price: 4.15,
+        category: categories.desserts
+    },
+    {
+        imgSource: "images/Champagne.jpg",
+        name: "Champagne",
+        description: "A delicious taste",
+        price: 7,
+        category: categories.beverages
+    },
+    {
+        imgSource: "images/Espresso.jpg",
+        name: "Espresso",
+        description: "Made with finely ground coffee beans",
+        price: 2.5,
+        category: categories.beverages
     },
 ]
